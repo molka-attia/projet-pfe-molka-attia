b2dc181b038a53254cb4ec62400b33f15a88ea27
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-07 18:15:23 +0100

    api users read

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 
 const app = express();
+app.use((req,res,next)=>{
+res.setHeader('Access-Control-Allow-Origin','*');
+res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,Content, Accept ,Content-Type,Authorization');
+res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PATCH,OPTIONS');
+next();
+});
 app.get('/api/users',(req,res)=>{const users = [
     {
       id:"1",
-      name: 'John Doe',
+      name: 'Molka',
       user_img:"4c.jpg",
       email: 'john.doe.gmail.com',
       type:'admin',
@@ -13,7 +19,7 @@ app.get('/api/users',(req,res)=>{const users = [
     },
     {
       id:"2",
-      name: 'John Doe2',
+      name: 'Molka2',
       user_img:"4c.jpg",
       email: 'john.doe.gmail.com2',
       type:'admin',
