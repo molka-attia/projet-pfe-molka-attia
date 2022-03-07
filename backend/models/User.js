c0a857d246bd45732a493c350d08d310b94cf5b5
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-07 18:55:45 +0100

    mongoose model

---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: false},
+    email: {type: String, required: false, unique: true},
+    password: {type: String, required: false},
+    type:{type: String, required: false},
+    user_img:{type: String, required: false},
+});
+
+module.exports = mongoose.model('User', userSchema);
