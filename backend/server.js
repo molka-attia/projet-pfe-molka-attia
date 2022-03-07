f12c975de9cfc71f6578392d852d391593ccaa6f
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-07 15:51:59 +0100

    fixing error

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const http = require('http');
 const app = require('./app');
 
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
+
+
 // make server listen on port
 server.listen(3000);
