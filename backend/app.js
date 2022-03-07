f2864e78da9aad2dfa41ad0ee35ca0bfea78bc03
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-07 18:25:25 +0100

    Connect app to mongo

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connecting to database
+mongoose.connect('mongodb://localhost:27017/Pfe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected successfully to MongoDB !'))
+  .catch(() => console.log('Connection failed to MongoDB !'));
+
+
+
+
 app.use((req,res,next)=>{
 res.setHeader('Access-Control-Allow-Origin','*');
 res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,Content, Accept ,Content-Type,Authorization');
