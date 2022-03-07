25744fe1127cec8534746156d937342d8e0b4d03
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-07 16:09:36 +0100

    create get users api

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,32 @@ const express = require('express');
 
 
 const app = express();
-app.use((req,res)=>{res.json("hello mimi")});
+app.get('/api/users',(req,res)=>{const users = [
+    {
+      id:"1",
+      name: 'John Doe',
+      user_img:"4c.jpg",
+      email: 'john.doe.gmail.com',
+      type:'admin',
+    
+    },
+    {
+      id:"2",
+      name: 'John Doe2',
+      user_img:"4c.jpg",
+      email: 'john.doe.gmail.com2',
+      type:'admin',
+    },  
+    {
+      id:"3",
+      name: 'Emily',
+      user_img:"4c.jpg",
+      email: 'john.doe.gmail.com2',
+      type:'technicien',
+    },  
+  ];
+  res.json(users);
+  });
+
 
 module.exports = app;
