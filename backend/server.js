ad75493c0fe7f3b2a9559bec5c48f8a4a01b3c1b
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-07 15:37:41 +0100

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-res.end('hellow mimi');
+res.end('hellow mimi2');
 });
 // make server listen on port
 server.listen(3000);
