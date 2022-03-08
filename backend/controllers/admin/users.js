2e6911596deb188f08d7ba8c42bdeeeaa734da58
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-08 17:20:10 +0100

    fixing with controller

---
+++ backend/controllers/admin/users.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const users = require('../../models/User');
+
+
+
+exports.getUsers = (req, res, next) =>{
+    users.find({},{_id:1,name:1,email:1,password:1,type:1,user_img:1})
+    .then(userResults => {res.json(userResults);console.log(userResults)});
+}
+// exports.getOneUser = (req, res, next) =>{
+//     users.find({_id:req.params.id},{_id:1,name:1,email:1,password:1,type:1,user_img:1})
+//     .then(userResults => res.json(userResults[0]));
+// }
+
+// exports.delete= (req, res, next) =>{
+// users.deleteOne({_id:req.params.id})
+// .then(userResults => res.json("succes"));
+// }
+
+// exports.getStats = (req, res, next) => {
+//   // users.find({},{_id:1})
+// //    users.size().then(stats => {
+// //                 res.json(stats);      
+// //             });
+
+//     users.aggregate([
+//         // {$match:{_id :mongoose.Types.ObjectId()}},
+//         // {$project : {"users" : {$size :"$users"},_id:0}}
+//        { $group:{_id:null, users:{$sum:1}}}
+//         ])
+//         .then(stats => {
+//             res.json(stats[0]);      
+//         });
+
+//    }
+//    exports.getStatsclubs = (req, res, next) => {
+//     // users.find({},{_id:1})
+//   //    users.size().then(stats => {
+//   //                 res.json(stats);      
+//   //             });
+  
+//       clubs.aggregate([
+//           // {$match:{_id :mongoose.Types.ObjectId()}},
+//           // {$project : {"users" : {$size :"$users"},_id:0}}
+//          { $group:{_id:null, clubs:{$sum:1}}}
+//           ])
+//           .then(stats => {
+//               res.json(stats[0]);      
+//           });
+  
+//      }
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const User=require('./../models/User');
+const userController = require('../controllers/admin/users');
 
-router.get('/',(req,res)=>{
-    User.find().then(users=>res.status(200).json(users)).catch(err=>res.status(400).json('Eror')); 
-   })
+// router.get('/',(req,res)=>{
+//     User.find().then(users=>res.status(200).json(users)).catch(err=>res.status(400).json('Eror')); 
+//    })
 
+   router.get('/',userController.getUsers);
 
 module.exports = router;
