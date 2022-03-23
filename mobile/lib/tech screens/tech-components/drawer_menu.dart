e7a0a5ff35cf6e1429a0b294bcc7e4a061a3961f
molka-attia <81327438+molka-attia@users.noreply.github.com>
2022-03-23 12:54:36 +0100

    logout as different users

--- mobile/lib/tech screens/tech-components/drawer_menu.dart
+++ mobile/lib/tech screens/tech-components/drawer_menu.dart
@@ -2,17 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:responsive_admin_dashboard/constants/constants.dart';
 import 'package:responsive_admin_dashboard/AllusersAdmin.dart';
 // import 'package:responsive_admin_dashboard/screens/components/Allticketsadmin.dart';
-import 'package:responsive_admin_dashboard/AllticketsTech.dart';
-import 'package:responsive_admin_dashboard/tech screens/tech-components/drawer_list_tile.dart';
-import 'package:responsive_admin_dashboard/tech screens/tech_dash_board_screen.dart';
+import 'package:responsive_admin_dashboard/AllticketsAdmin.dart';
+import 'package:responsive_admin_dashboard/main%20copy.dart';
 import 'package:responsive_admin_dashboard/pages/login_page.dart';
-import 'package:responsive_admin_dashboard/techmainDash.dart';
+import 'package:responsive_admin_dashboard/screens/components/drawer_list_tile.dart';
 
+import 'package:flutter/cupertino.dart';
+
+
+// import 'package:home_rental/singup.dart';
+// import 'package:home_rental/clubForm.dart';
+
+
+
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:responsive_admin_dashboard/mainDash.dart';
 
-class DrawerMenu extends StatelessWidget {
-  const DrawerMenu({Key key}) : super(key: key);
+import '../../AllticketsTech.dart';
+import '../../AllticketsUser.dart';
+import '../../techmainDash.dart';
+import '../../usermainDash.dart';
+
+class DrawerMenu extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<DrawerMenu> {
+  PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  var type;
+  getLoggedUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String tt = prefs.getString("type");
+    setState(() {
+      type = tt;
+    });
+    print(prefs.getString("type"));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLoggedUser();
+  }
+
+  Logout() async {
+    //clear shared preferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>LoginUiApp() ));
+  }
+
+  //const DrawerMenu({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +68,7 @@ class DrawerMenu extends StatelessWidget {
             height: 100,
             child: Image.asset("assets/images/cartecoloredlogo.png"),
           ),
+           if (type == "technicien")
           DrawerListTile(
               title: 'Dash Board',
               svgSrc: 'assets/icons/Dashboard.svg',
@@ -34,17 +79,65 @@ class DrawerMenu extends StatelessWidget {
                                   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>TechMainDash() ));
                                }
               ),
-         
+          if (type == "technicien")
                DrawerListTile(
               title: 'tickets', svgSrc: 'assets/icons/Pages.svg', tap: () {
                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>AllticketsTech() ));
               }),
           // DrawerListTile(
           //     title: 'Message', svgSrc: 'assets/icons/Message.svg', tap: () {}),
+           if (type == "technicien")
           DrawerListTile(
               title: 'Statistics',
               svgSrc: 'assets/icons/Statistics.svg',
               tap: () {}),
+            if (type == "admin")
+          DrawerListTile(
+              title: 'Dash Board',
+              svgSrc: 'assets/icons/Dashboard.svg',
+              tap: () { 
+                                  //After successful login we will redirect to profile page. Let's create profile page now
+                                  // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage()));
+                                  
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>MainDash() ));
+                               }
+              ),
+                if (type == "admin")
+          DrawerListTile(
+              title: 'Users',
+              svgSrc: 'assets/icons/Subscribers.svg',
+              tap: () {
+
+                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>AllusersAdmin() ));
+              }),
+               if (type == "admin")
+               DrawerListTile(
+              title: 'tickets', svgSrc: 'assets/icons/Pages.svg', tap: () {
+                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>AllticketsAdmin() ));
+              }),
+          // DrawerListTile(
+          //     title: 'Message', svgSrc: 'assets/icons/Message.svg', tap: () {}),
+           if (type == "admin")
+          DrawerListTile(
+              title: 'Statistics',
+              svgSrc: 'assets/icons/Statistics.svg',
+              tap: () {}),
+              if (type == "utilisateur")
+              DrawerListTile(
+              title: 'Profile',
+              svgSrc: 'assets/icons/Dashboard.svg',
+              tap: () { 
+                                  //After successful login we will redirect to profile page. Let's create profile page now
+                                  // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage()));
+                                  
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>userMainDash() ));
+                               }
+              ),
+         if (type == "utilisateur")
+               DrawerListTile(
+              title: 'tickets', svgSrc: 'assets/icons/Pages.svg', tap: () {
+                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>AllticketsUser() ));
+              }),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: appPadding * 2),
             child: Divider(
@@ -60,7 +153,10 @@ class DrawerMenu extends StatelessWidget {
           DrawerListTile(
               title: 'Logout',
               svgSrc: 'assets/icons/Logout.svg',
-              tap: () {}),
+              tap: () {
+              Logout();
+
+              }),
         ],
       ),
     );
